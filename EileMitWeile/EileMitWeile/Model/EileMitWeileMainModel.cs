292c1086bcec6a8c530173ea11f6a7cc714fe8ae
bannwafa <33682594+bannwafa@users.noreply.github.com>
2019-01-06 16:36:47 +0100

    Würfeln nun möglich

--- EileMitWeile/EileMitWeile/Model/EileMitWeileMainModel.cs
+++ EileMitWeile/EileMitWeile/Model/EileMitWeileMainModel.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace EileMitWeile.Model
 {
-	public class EileMitWeileMainModel
-	{
-	}
+    public class EileMitWeileMainModel
+    {
+        public string Dice()
+        {
+            Random r = new Random();
+            return r.Next(0, 6).ToString();
+        }
+    }
 }
